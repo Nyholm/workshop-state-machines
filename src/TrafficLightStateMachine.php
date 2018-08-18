aa64f02c0bd8661aabebfff61572340a4b02d7ba
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-08-18 18:25:00 +0200

    Added implementation of TrafficLightStateMachine

--- src/TrafficLightStateMachine.php
+++ src/TrafficLightStateMachine.php
@@ -15,7 +15,16 @@ class TrafficLightStateMachine
      */
     public function can(string $transition): bool
     {
-        // TODO write me
+        switch ($this->state) {
+            case 'green':
+                return ($transition === 'to_yellow');
+            case 'yellow':
+                return ($transition === 'to_green' || $transition === 'to_red');
+            case 'red':
+                return ($transition === 'to_yellow');
+            default:
+                return false;
+        }
     }
 
     /**
@@ -25,6 +34,23 @@ class TrafficLightStateMachine
      */
     public function apply(string $transition): void
     {
-        // TODO write me
+        if (!$this->can($transition)) {
+            throw new \InvalidArgumentException('Invalid transition');
+        }
+
+        switch ($this->state) {
+            case 'green' && ($transition === 'to_yellow'):
+                $this->state = 'yellow';
+                break;
+            case 'yellow' && ($transition === 'to_green'):
+                $this->state = 'green';
+                break;
+            case 'yellow' && ($transition === 'to_red'):
+                $this->state = 'red';
+                break;
+            case 'red' && ($transition === 'to_yellow'):
+                $this->state = 'yellow';
+                break;
+        }
     }
 }
