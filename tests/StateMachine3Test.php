a69305180a4eb37cd97876214f8fc01d225af1a5
Nyholm <tobias.nyholm@gmail.com>
2018-08-28 08:46:01 +0200

    Updated test

--- tests/StateMachine3Test.php
+++ tests/StateMachine3Test.php
@@ -4,24 +4,42 @@ declare(strict_types=1);
 
 namespace Tests\App;
 
-use App\TrafficLightStateMachine;
+use App\Entity\TrafficLight;
+use App\StateMachine;
 use Nyholm\NSA;
 use PHPUnit\Framework\TestCase;
 
-class TrafficLightStateMachineTest extends TestCase
+class StateMachine3Test extends TestCase
 {
+    /** @var  StateMachine */
+    private $sm;
+    protected function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+        $this->sm = new StateMachine( [
+            'green' => [
+                'to_yellow' =>  'yellow',
+            ],
+            'yellow' => [
+                'to_green' => 'green',
+                'to_red' => 'red',
+            ],
+            'red' => [
+                'to_yellow' =>  'yellow',
+            ],
+        ]);
+    }
+
     /**
      * @dataProvider canProvider
      */
     public function testCan(string $currentState, string $transition, bool $result)
     {
-        $sm = new TrafficLightStateMachine();
-
-        NSA::setProperty($sm, 'state', $currentState);
+        $trafficLight = new TrafficLight($currentState);
         if ($result) {
-            $this->assertTrue($sm->can($transition), sprintf('We should be allowed to do transition "%s" when in state "%s"', $transition, $currentState));
+            $this->assertTrue($this->sm->can($trafficLight, $transition), sprintf('We should be allowed to do transition "%s" when in state "%s"', $transition, $currentState));
         } else {
-            $this->assertFalse($sm->can($transition), sprintf('We should NOT be allowed to do transition "%s" when in state "%s"', $transition, $currentState));
+            $this->assertFalse($this->sm->can($trafficLight, $transition), sprintf('We should NOT be allowed to do transition "%s" when in state "%s"', $transition, $currentState));
         }
     }
 
@@ -30,17 +48,14 @@ class TrafficLightStateMachineTest extends TestCase
      */
     public function testApply(string $currentState, string $newState, string $exception = null)
     {
-        $sm = new TrafficLightStateMachine();
+        $trafficLight = new TrafficLight($currentState);
         if (null !== $exception) {
             $this->expectException($exception);
         }
 
-        NSA::setProperty($sm, 'state', $currentState);
-        $sm->apply(sprintf('to_%s', $newState));
-        $this->assertEquals($newState, NSA::getProperty($sm, 'state'), sprintf('A state machine with state "%s" should have updated to "%s" after ->apply("%s")', $currentState, $newState, $newState));
+        $this->sm->apply($trafficLight, sprintf('to_%s', $newState));
+        $this->assertEquals($newState, $trafficLight->getState(), sprintf('A state machine with state "%s" should have updated to "%s" after ->apply("%s")', $currentState, $newState, $newState));
     }
-
-
 
     public function canProvider()
     {
